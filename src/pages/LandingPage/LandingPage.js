80489a4b341c4e2fd1eff8ce286ce89d9bd7f728
nurmdrafi <nurmdrafi2014@gmail.com>
2022-09-13 13:31:47 +0600

    landing page updated

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useUserAuth from "../../context/UserAuthContext";
 
 const LandingPage = () => {
+  const { authUser } = useUserAuth();
+
   return (
     <div className="bg-gradient-to-r from-rose-50 to-teal-50 dark:bg-gradient-to-b dark:from-slate-900 dark:to-slate-900 flex min-h-[calc(100vh-65px)] items-center justify-center">
       <div>
         <h1 className="text-primary font-bold text-center text-7xl">
           You can write anything...
         </h1>
-        <div className="flex justify-center gap-5 mt-5">
-          <Link to="/login">
-            <button className="btn btn-primary btn-outline ">Login</button>
-          </Link>
-          <Link to="/registration">
-            <button className="btn btn-primary btn-outline">Register</button>
-          </Link>
-        </div>
+        {!authUser?.auth ? (
+          <div className="flex justify-center gap-5 mt-5">
+            <Link to="/login">
+              <button className="btn btn-primary btn-outline ">Login</button>
+            </Link>
+            <Link to="/registration">
+              <button className="btn btn-primary btn-outline">Register</button>
+            </Link>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center">
+            <Link to="/home">
+              <button className="btn btn-primary btn-outline">
+                Go To Home Page
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
